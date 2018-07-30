5d66811ed46314ac569232d6a05a645be314e73f
pavelnv <np_v@bk.ru>
2018-07-30 17:31:04 +0500

    добавление json

--- client.py
+++ client.py
@@ -1,14 +1,17 @@
 import socket
+import json
 
 sock = socket.socket()
 
-sock.connect(('localhost', 9040))
+sock.connect(('localhost', 9090))
 
 while True:
     data = sock.recv(1024)
     if len(data) == 0:
         break
 
-    print(data.decode("utf-8"))
+    result = data.decode("utf-8")
 
 sock.close()
+
+print(json.loads(result))
--- server.py
+++ server.py
@@ -1,14 +1,21 @@
 import socket
+import json
 
 sock = socket.socket()
 
-sock.bind(('', 9040))
+sock.bind(('', 9090))
 
 sock.listen(1)
 
+data = {
+    "msg": "Hello",
+    "action": 'do smth'
+}
+
+s_data = json.dumps(data)
+
 while True:
     client, addr = sock.accept()
-    print(f"Получен запрос на соединение от (str(addr))")
-    client.send('Hello world!'.encode("utf-8"))
-    client.send('Hello'.encode("utf-8"))
+    print(f"Получен запрос на соединение от {str(addr)}")
+    client.send(s_data.encode("utf-8"))
     client.close()
