c0be55daf18cc0a58c29057addbaed83186e5025
pavelnv <np_v@bk.ru>
2018-07-30 17:05:57 +0500

    добавлены тесты

--- my_tests.py
+++ my_tests.py
@@ -1,5 +1,5 @@
 import unittest
-from my_library import power_two
+from my_library import power_two, mult_nums
 
 class TestMyCase(unittest.TestCase):
 
@@ -13,6 +13,13 @@ class TestMyCase(unittest.TestCase):
         self.assertEqual(power_two(5), 25)
         self.assertEqual(power_two(-15), 225)
 
+    def test_mult_nums(self):
 
+        self.assertEqual(mult_nums(1,2), 2)
+        self.assertEqual(mult_nums(14, 2), 28)
+        self.assertEqual(mult_nums(111, -12), -1332)
+
+    def test_integration(self):
+        self.assertEqual(power_two(3), mult_nums(3, 3))
 
 unittest.main()
